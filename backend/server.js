c817a5fab0729e8ff99d85dfd69a3bfaa83b61db
Aditya Warekar <adityawarekar1984@gmail.com>
2026-06-19 23:40:29 +0530

    Made folder Structure

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB =
+  require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+  res.send("Social Media API Running");
+});
+
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/posts",
+  require("./routes/postRoutes")
+);
+
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+  console.log(
+    `Server running on ${PORT}`
+  );
+});
