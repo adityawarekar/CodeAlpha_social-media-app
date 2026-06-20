cae7c9b6788ab0fe8753a1d90b2e869400f5fb22
Aditya Warekar <adityawarekar1984@gmail.com>
2026-06-20 11:41:18 +0530

    Setup social media app backend and frontend routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Feed from "./pages/Feed";
+
+function App() {
+
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Feed />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
+
+}
+
+export default App;
